ac348814bc8b4ae199276c020262d29a3d3da99a
InyoungHuh <iyoungsh@gmail.com>
2020-03-29 20:55:47 -0700

    delete comment

--- databaseconnection.py
+++ databaseconnection.py
@@ -1,32 +1,10 @@
-#from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 import psycopg2
 import io
 
-# table_name = 'financial_statement_quarterly'
-# try:
-#   connection = psycopg2.connect(user="postgres",
-#                                 password="11!Tosel",
-#                                 host="localhost",
-#                                 port="5432",
-#                                 database="stockprice")
-#
-#   cursor = connection.cursor()
-#   postgreSQL_select_Query = "select * from %s" %(table_name)
-#   cursor.execute(postgreSQL_select_Query)
-#   records = cursor.fetchall()
-#   #df = pd.DataFrame(data=records,columns=['Date', 'High', 'Low', 'Open', 'Close','Volume','Adj_Close'])
-# except (Exception, psycopg2.Error) as error :
-#   print ("Error while fetching data from PostgreSQL", error)
-# finally:
-#   #closing database connection.
-#   if(connection):
-#     cursor.close()
-#     connection.close()
-#     print("PostgreSQL connection is closed")
-
 
 class DatabaseConnection:
-    dbc = {"user":"tmpuser", "password":"mypassword", "host":"localhost", "port":"5432", "database":"stockprice"}
+    dbc = {"user": "tmpuser", "password": "mypassword", "host": "localhost", "port": "5432", "database": "stockprice"}
 
     def __init__(self):
         self._conn = psycopg2.connect(**self.dbc)
@@ -63,12 +41,10 @@ class DatabaseConnection:
         self.cursor.execute(sql, params or ())
         return self.fetchall()
 
+
 with DatabaseConnection() as db:
     table_name = 'financial_statement_quarterly'
     postgreSQL_select_Query = "select * from %s" % (table_name)
     db.execute(postgreSQL_select_Query)
     db.fetchall()
     print("success")
-#    db.execute('INSERT INTO comments (username, comment_body, date_posted) VALUES (?, ?, current_date)', ('tom', 'this is a comment'))
-#    comments = db.query('SELECT * FROM comments')
-#    print(comments)
